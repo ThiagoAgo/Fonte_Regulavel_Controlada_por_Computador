daf036e42c28455bd3cf8e2d7161b3b5bedc47ae
ThiagoAgo <116441661+ThiagoAgo@users.noreply.github.com>
2023-05-18 22:53:15 -0300

    Atualização 5
    # Corrigido erro que impedia alterar a tensão a partir da porta serial
    #PWM (Resolução aumentada de 8 para 12 bits).
    #PWM (Frequência aumentada de 978 para 3909 Hz).
    # Mudança nos pinos (5 -->9) e (6 -->10) .

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -14,15 +14,39 @@
 
   Criado 08/04/2023.    
   Atualizado 11/05/2023.
+  Atualizado 18/05/2023.
+  
+  Referencias:
+  https://forum.arduino.cc/t/16bit-7khz-pwm-output/547229/15
 
 */
-#define Saida_A 5
-#define Saida_B 6
+const unsigned int TOP = 0x0FFF;//4095(dec) // PWM de 12 bits a 3906 Hz, se prescaler = 1 
 
-unsigned int tensao_saida[] = {0, 3300}; //tensao de saida em mV 
+unsigned int tensao_saida[] = {5000, 3300}; //tensao de saida em mV 
 enum {INICIO, DEZENA_DE_MILHAR, MILHAR, CENTENA, DEZENA, UNIDADE,FIM} estMQ; // definindo os estados para a maquina de estados
 byte DEZENA_DE_MILHAR1, MILHAR1, CENTENA1, DEZENA1, UNIDADE1;
 byte SaidaEscolida;
+
+void PWMconfig(){
+
+  TCCR1A = 0;               // Configura com  "Normal port operation, OC1A/OC1B disconnected".
+  TCCR1B = 0;               // Configura com  "Normal port operation, OC1A/OC1B disconnected".
+  TIMSK1 = 0;  // Timer/Counter1 Interrupt Mask Register
+  TIFR1 = 0;   // Timer/Counter1 Interrupt Flag Register
+  ICR1 = TOP;
+  OCR1A = 0;  // Pwm no pino 9 com duty circle em 0%
+  OCR1B = 0;  // Pwm no pino 10 com duty circle em 0%
+  TCCR1B |= (1 << CS10); // prescaler = 1
+  TCCR1A |= (1 << WGM11);   // Configura o PWM no modo "fast PWM" com TOP = ICR1
+  TCCR1B |= (1 << WGM13) | (1 << WGM12) ; // Configura o PWM no modo "fast PWM" com TOP = ICR1
+    // Enable Fast PWM on Pin 9: Set OC1A at BOTTOM and clear OC1A on OCR1A compare
+  TCCR1A |= (1 << COM1A1);
+  pinMode(9, OUTPUT);
+  // Enable Fast PWM on Pin 10: Set OC1B at BOTTOM and clear OC1B on OCR1B compare
+  TCCR1A |= (1 << COM1B1);
+  pinMode(10, OUTPUT);   
+  
+}
 
 void msg_incoreta(){
   char n;
@@ -59,16 +83,22 @@ void Tensao_requisitada(int a,int  b,int  c,int  d,int  e){
 }
 
 void processaMQ(char m){
+  
   byte valor = m - 48;  
   switch(estMQ){
     case INICIO:
-      if(m=='A' || m =='B'){
+      if(m=='A'){
       estMQ = DEZENA_DE_MILHAR;
       SaidaEscolida = m;
-      }break;
+      break;
+      }else if(m=='B'){
+      estMQ = DEZENA_DE_MILHAR;
+      SaidaEscolida = m;
+      break;
+      }
     
     case DEZENA_DE_MILHAR: // estado para Esperar mensagem iniciar   
-      if (valor > -1 & valor < 7){
+    	if (valor > -1 & valor < 7){
         DEZENA_DE_MILHAR1  = valor;
         estMQ = MILHAR;  
       }
@@ -78,7 +108,7 @@ void processaMQ(char m){
       }break;
     
       case MILHAR: // estado para Esperar mensagem iniciar   
-      if (valor > -1 & valor < 10){
+    	if (valor > -1 & valor < 10){
         MILHAR1  = valor;
         estMQ = CENTENA;  
       }
@@ -88,7 +118,7 @@ void processaMQ(char m){
       }break;
 
       case CENTENA: // estado para Esperar mensagem iniciar   
-      if (valor > -1 & valor < 10){
+    	if (valor > -1 & valor < 10){
         CENTENA1  = valor;
         estMQ = DEZENA;  
       }
@@ -98,7 +128,7 @@ void processaMQ(char m){
       }break;
 
       case DEZENA: // estado para Esperar mensagem iniciar   
-      if (valor > -1 & valor < 10){
+    	if (valor > -1 & valor < 10){
         DEZENA1  = valor;
         estMQ = UNIDADE;  
       }
@@ -108,7 +138,7 @@ void processaMQ(char m){
       }break; 
 
       case UNIDADE: // estado para Esperar mensagem iniciar   
-      if (valor > -1 & valor < 10){
+    	if (valor > -1 & valor < 10){
         UNIDADE1  = valor;
         estMQ = FIM;  
       }
@@ -122,7 +152,7 @@ void processaMQ(char m){
       Tensao_requisitada(DEZENA_DE_MILHAR1, MILHAR1, CENTENA1, DEZENA1, UNIDADE1);
       mudarTensaodeSaida_A();
       mudarTensaodeSaida_B();       
-      Serial.println(" Os dados foram salvos" +(String)tensao_saida[0] +(String)tensao_saida[1]);
+      Serial.println(" Os dados foram salvos" +(String)tensao_saida[0] +" E "+(String)tensao_saida[1]);
      }
     else{
       Serial.println(" BYTE de filalizaçao incorreto, nada foi alterado.");
@@ -143,21 +173,20 @@ void serialEvent(){
 
 // E interesantes usar funcoes diferentes para cada saida para que seja possivel usar saidas com "resolucoes" e ruidos menores
 void mudarTensaodeSaida_A(){
-  int v_out = int(tensao_saida[0]/129); // 129 e ((V_porta)(ganho Amp-op))/ResolucaoPWM ===> (5 x 6.6)/255   (em milivolts)
-  if  (v_out > 255) v_out = 255;
-  analogWrite(Saida_A, v_out);
+  int v_out = int(tensao_saida[0]/7.3260); // 7.3260 e ((V_porta)(ganho Amp-op))/ResolucaoPWM ===> (5 x 6)/4095   (em milivolts)
+  if  (v_out > TOP) v_out = TOP;
+  OCR1A = v_out; // Altera o duty circle do PWM do pino 9
 }
 
 void mudarTensaodeSaida_B(){
-  int v_out = int(tensao_saida[1]/62); // 62 e ((V_porta)(ganho Amp-op))/ResolucaoPWM ===> (5 x 3.2)/255   (em milivolts) 
-  if  (v_out > 255) v_out = 255;
-  analogWrite(Saida_B, v_out);
+  int v_out = int(tensao_saida[1]/3.9072); // 3.9072 e ((V_porta)(ganho Amp-op))/ResolucaoPWM ===> (5 x 3.2)/4095   (em milivolts) 
+  if  (v_out > TOP) v_out = TOP;
+  OCR1B = v_out; // Altera o duty circle do PWM do pino 10
 }
 
 void setup() {
-  pinMode(Saida_A, OUTPUT);
-  pinMode(Saida_B, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+  PWMconfig();
   mudarTensaodeSaida_A();
   mudarTensaodeSaida_B();
   Serial.begin(9600);
@@ -165,10 +194,5 @@ void setup() {
 
 
 void loop() {  
-  
-  // Somente para indicar que o programa esta funcionando corretamente.
-  digitalWrite(LED_BUILTIN, HIGH);  
-  delay(1000);                      
-  digitalWrite(LED_BUILTIN, LOW);   
-  delay(1000);
+
 }
